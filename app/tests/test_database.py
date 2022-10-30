61789937230b0bd98720d8796f9df63d6d3ec6e0
Halimahfet <halimah.fetuga@postgrad.manchester.ac.uk>
2022-10-30 21:23:46 +0000

    Add files via upload

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -25,18 +25,10 @@ class DatabaseTests(unittest.TestCase):
     def test_get_total_number_items(self):
         """Test that the total number of items returns the correct value."""
         self.assertEquals(self.db_mod.get_total_number_items(), 8218165, 'Test total items returns correct value')
-    
-    def test_get_average_ACT(self):
-        """Test that the average of actual cost returns the correct value."""
-        self.assertEquals(self.db_mod.get_average_ACT(), 76.221, 'Test average act cost returns correct value')
 
-    def test_get_percentage_of_top_item(self):
-        """Test that the percentage of top item returns the correct value."""
-        self.assertEquals(self.db_mod.get_percentage_of_top_item(), 2.75, 'Test top item returns correct value')
-
-    def test_get_unique_number_of_items(self):
-        """Test that the unique number of items returns the expected value."""
-        self.assertEquals(self.db_mod.get_unique_number_of_items(), 13935, 'Test_for_expected value')
+    def test_get_top_prescribed_item(self):
+        """Test that the top prescribed item returns the correct value."""
+        self.assertEquals(self.db_mod.get_top_prescribed_item(), 'Omeprazole_Cap E/C 20mg', 'Test top prescribed item returns correct value')
 
 if __name__ == "__main__":
     unittest.main()
