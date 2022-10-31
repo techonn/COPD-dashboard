debdf5abc9d6e45296d032220aba2a15e18d4011
eedrobup <pubordee.a@gmail.com>
2022-10-31 07:30:44 +0000

    Revert "Add files via upload"
    This reverts commit 4fc45ac1220b679f89476a66ecc8fcf6d68f8a3b.

--- app/database/controllers.py
+++ app/database/controllers.py
@@ -9,7 +9,6 @@ DESCRIPTION:   Contains the Database class that contains all the methods used fo
 
 from sqlalchemy.sql import func
 from flask import Blueprint
-from sqlalchemy import desc
 
 from app import db
 from app.database.models import PrescribingData, PracticeData
@@ -33,8 +32,21 @@ class Database:
     def get_n_data_for_PCT(self, pct, n):
         """Return all the data for a given PCT."""
         return db.session.query(PrescribingData).filter(PrescribingData.PCT == pct).limit(n).all()
+    
+    def get_average_ACT(self):
+        """Return Average ACT cost """
+        return db.session.query(func.avg(PrescribingData.ACT_cost)).first()[0]
 
-    def get_top_prescribed_item(self):
-        """ Return top prescribed drug."""
-        return db.session.query(PrescribingData).group_by(PrescribingData.BNF_name).order_by(desc(PrescribingData.items)).first().BNF_name
+    def get_number_of_top_BNF_item(self):
+        """Return the top quantity of prescribed item among BNF codes."""
+        return db.session.query(func.sum(PrescribingData.items)).\
+            group_by(PrescribingData.BNF_code).\
+            order_by(func.sum(PrescribingData.items).desc()).first()[0]
 
+    def get_percentage_of_top_item(self):
+        percentage = self.get_number_of_top_BNF_item()/self.get_total_number_items()
+        return round(float(percentage)*100, 2)
+    
+    def get_unique_number_of_items(self):
+        """Return the number of unique items."""
+        return (db.session.query(PrescribingData.BNF_code).distinct().count())
